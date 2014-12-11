8a5deb9e0ab22feb3198e5e914760b398060ed11
Chris Cannam <c.cannam@qmul.ac.uk>
2014-12-11 13:55:25 +0000

    Start DistanceMetric test

---
+++ test/TestDistanceMetric.cpp
@@ -0,0 +1,41 @@
+
+#include "DistanceMetric.h"
+
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MAIN
+
+#include <boost/test/unit_test.hpp>
+
+static vector<double> getTestFeature(double m)
+{
+    vector<double> f;
+    double fd[] = { 0, 1, 2, 3 };
+    for (int i = 0; i < 4; ++i) {
+	f.push_back(fd[i] * m);
+    }
+    return f;
+}
+
+BOOST_AUTO_TEST_SUITE(TestDistanceMetric)
+
+BOOST_AUTO_TEST_CASE(nonorm)
+{
+    DistanceMetric dm(DistanceMetric::NoDistanceNormalisation);
+    vector<double>
+	e1 = getTestFeature(1),
+	e2 = getTestFeature(2),
+	e0 = getTestFeature(0);
+
+    BOOST_CHECK_EQUAL(dm.calcDistance(e0, e0), 0.0);
+}
+
+BOOST_AUTO_TEST_SUITE_END()
+
+
+    
